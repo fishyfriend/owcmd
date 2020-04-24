af0f79c16f573f4dbaafe77a430d7621857e706b
Jacob First <jacob.first@member.fsf.org>
2020-04-24 19:14:31 -0400

    Support isearch and query-replace

--- owcmd.el
+++ owcmd.el
@@ -77,8 +77,12 @@
 ;;
 ;; Configuration:
 ;;
-;; You can configure the package's behavior through variables whose names
-;; are prefixed with "owcmd-".
+;; You can configure the package's behavior through variables whose
+;; names are prefixed with "owcmd-".  Here is one suggested
+;; customization to try:
+;;
+;;   ;; Use C-g to abort owcmd and stay in the selected window.
+;;   (add-to-list 'owcmd-cancel-reselect-commands 'keyboard-quit)
 
 ;;; Code:
 
@@ -87,26 +91,33 @@
   :group 'owcmd
   :type 'boolean)
 
-(defcustom owcmd-cancel-reselect-commands '("^isearch-.*"
-                                            "^query-replace\\(-regexp\\)?$"
-                                            recentf-open-file
+(defcustom owcmd-cancel-reselect-commands '(recentf-open-file
                                             recentf-open-files)
   "List of commands that cancel reselection of the original window.
-Each item is a symbol or string; if a string, it will be interpreted as a
-regexp.  This setting is applied by checking the current command name as
+The list should match the specification accepted by
+`owcmd-member-cmdlist'.
+
+This setting is applied by checking the current command name as
 reported by `this-command' in a post-command hook."
   :group 'owcmd
   :type '(repeat (choice symbol regexp)))
 
-(defcustom owcmd-ignore-commands '(owcmd-execute-other-window
-                                   ;; god-mode compatibility
-                                   god-mode-self-insert)
+(defcustom owcmd-ignore-commands
+  '(owcmd-execute-other-window
+    "^isearch-.*"
+    (not . "^isearch-\\(abort\\|cancel\\|exit\\)$")
+    "^query-replace\\(-regexp\\)?$"
+    ;; god-mode compatibility
+    god-mode-self-insert)
   "List of commands to ignore, checked just after a command completes.
-Each item is a symbol or string; if a string, it will be interpreted as a
-regexp.  Commands in this list will not trigger reselection of the
-original window and will not prevent a subsequent command from doing so.
-This setting is applied by checking the current command name as reported
-by `this-command' in a post-command hook.
+The list should match the specification accepted by
+`owcmd-member-cmdlist'.
+
+Commands in this list will not trigger reselection of the
+original window and will not prevent a subsequent command from
+doing so.  This setting is applied by checking the current
+command name as reported by `this-command' in a post-command
+hook.
 
 If you wrap `owcmd-execute-other-window' in another command, you should
 add that command's name to `owcmd-ignore-commands'."
@@ -121,18 +132,20 @@ add that command's name to `owcmd-ignore-commands'."
                                           ;; evil-god-state compatibility
                                           evil-execute-in-god-state)
   "List of commands to ignore, checked just before any command is run.
-Each item is a symbol or string; if a string, it will be interpreted as a
-regexp.  Commands in this list will not trigger reselection of the
-original window and will not prevent a subsequent command from doing so.
-This setting is applied by checking the current command name as reported
-by `this-command' in a pre-command hook.
+The list should match the specification accepted by
+`owcmd-member-cmdlist'.
 
-Unsurprisingly, `owcmd-ignore-prefix-commands' is useful mainly for
-ignoring prefix commands (e.g. `digit-argument'), which alter the values
-of both `this-command' and `real-this-command' so that the actual command
-name can't be checked in a post-command hook.  For ignoring other
-commands that aren't prefix commands, you should generally use
-`owcmd-ignore-commands'."
+Commands in this list will not trigger reselection of the
+original window and will not prevent a subsequent command from
+doing so.  This setting is applied by checking the current
+command name as reported by `this-command' in a pre-command hook.
+
+Unsurprisingly, `owcmd-ignore-prefix-commands' is useful mainly
+for ignoring prefix commands (e.g. `digit-argument'), which alter
+the values of both `this-command' and `real-this-command' so that
+the actual command name can't be checked in a post-command hook.
+For ignoring other commands that aren't prefix commands, you
+should generally use `owcmd-ignore-commands'."
   :group 'owcmd
   :type '(repeat (choice symbol regexp)))
 
@@ -145,33 +158,47 @@ commands that aren't prefix commands, you should generally use
 (defvar owcmd--target-window nil
   "The window selected for running a command.")
 
-(defun owcmd--match-in-list (symbol listvar)
-  "Return t if SYMBOL is a match for any symbol or regexp in LISTVAR."
-  (let* ((items listvar)
-         (found nil))
-    (while (and items (not found))
-      (let ((item (car items)))
-        (setq found
-              (cond ((symbolp item) (eq symbol item))
-                    ((stringp item) (string-match item (symbol-name symbol)))
-                    (t nil))
-              items
-              (cdr items))))
-    found))
+(defun owcmd-member-cmdlist (command cmdlist)
+  "Return non-nil if COMMAND is included in CMDLIST.
+CMDLIST is a list of items which each take one of the following
+forms:
+
+  'SYMBOL        Include the command named SYMBOL.
+  \"REGEXP\"       Include all commands whose names match REGEXP.
+  '(not . ITEM)  Exclude commands matching ITEM, which must be a
+                 symbol or regexp string.  They are excluded even when
+                 another item in the list would include them."
+
+  (let (found bans)
+    (dolist (item cmdlist)
+      (pcase item
+        ((pred symbolp)
+         (unless found
+           (setq found (eq command item))))
+        ((pred stringp)
+         (unless found
+           (setq found (string-match item (symbol-name command)))))
+        ((and `(not . ,ban)
+              (guard (or (symbolp ban) (stringp ban))))
+         (push ban bans))
+        (_ (error "List format not recognized"))))
+    (and found
+         (or (not bans)
+             (not (owcmd-member-cmdlist command bans))))))
 
 (defun owcmd--handle-command-pre ()
   "Set up global state for handling prefix commands."
-  (when (owcmd--match-in-list this-command owcmd-ignore-prefix-commands)
+  (when (owcmd-member-cmdlist this-command owcmd-ignore-prefix-commands)
     (setq owcmd--ignore-prefix t)))
 
 (defun owcmd--handle-command-post ()
   "Select original window if appropriate, and update global state."
   (if owcmd--ignore-prefix
       (setq owcmd--ignore-prefix nil)
-    (unless (or (owcmd--match-in-list this-command owcmd-ignore-commands)
+    (unless (or (owcmd-member-cmdlist this-command owcmd-ignore-commands)
                 (minibuffer-window-active-p (selected-window)))
-      (when (and (not (owcmd--match-in-list this-command
-                                          owcmd-cancel-reselect-commands))
+      (when (and (not (owcmd-member-cmdlist this-command
+                                            owcmd-cancel-reselect-commands))
                  (or (eq (selected-window) owcmd--target-window)
                      (not owcmd-cancel-reselect-on-new-window))
                  (window-live-p owcmd--calling-window))
@@ -223,22 +250,44 @@ would trigger reselection of the original window.)
 If you wrap `owcmd-execute-other-window' in another command, you should
 add that command's name to `owcmd-ignore-commands'.
 
-Quirks:
+Notes about interaction with specific Emacs features:
 
-`owcmd-execute-other-window' selects windows using `select-window' with
-the NORECORD argument set to nil.  Thus, any actions that trigger on new
-window selection are likely to be triggered when using
-`owcmd-execute-other-window', both when the other window is selected and
-again when the original window is reselected.
+`execute-extended-command'.  You may observe a 4s delay in
+returning to the original window after executing a command via
+`execute-extended-command' (bound to \\<global-map>\\[execute-extended-command]).  If you type text or
+run any other command during the delay, the original window will
+be reselected immediately, so this issue should not interrupt
+your workflow.  (The odd behavior is due to an implementation
+quirk of the way `execute-extended-command' displays suggested
+keybindings for the command just run.  It is likely to be fixed
+in a future version of Emacs.)
 
-You may observe a 4s delay in returning to the original window after
-executing a command via `execute-extended-command' (bound to \\<global-map>\\[execute-extended-command]).
-If you type text or run any other command during the delay, the original
-window will be reselected immediately, so this issue should not interrupt
-your workflow.  (The odd behavior is due to an implementation quirk of
-the way `execute-extended-command' displays suggested keybindings for the
-command just run.  It is likely to be fixed in a future version of
-Emacs.)"
+Incremental search.  By default, owcmd ignores most incremental
+search commands (`isearch-forward' etc.), so that you can
+complete a full search \"session\" in the other window before
+jumping back.  Normally in Emacs, if you execute some other
+command like cursor movement during your search, that command
+causes the search to end.  Under owcmd, this is also true; just
+be aware that this \"extra\" command will be executed in the
+current window, and only afterwards will the original window will
+be reselected.  If you wish to exit a search session and jump
+back without taking any action, use `isearch-exit' (bound to \\<isearch-mode-map>\\[isearch-exit])
+or `keyboard-quit' (bound to \\<global-map>\\[keyboard-quit]), which owcmd does not ignore by
+default.
+
+`query-replace', `query-replace-regexp'.  Query replacement
+behaves similarly to incremental search, except that there's no
+dedicated exit command, so owcmd has no way to tell when the
+replacement is finished.  Thus, the next command after
+replacement will execute in the current window.  You should use
+`keyboard-quit' (bound to \\<global-map>\\[keyboard-quit])), or some other innocuous
+command, to return to the original window.
+
+`select-window'.  owcmd selects windows using `select-window'
+with the NORECORD argument set to nil.  Thus, any actions that
+trigger on new window selection are likely to be triggered when
+using `owcmd-execute-other-window', both when the other window is
+selected and again when the original window is reselected."
   (interactive)
   (let ((target-window (next-window (selected-window) 'no-minibuffer)))
     (cond ((eq target-window owcmd--calling-window) ; user has cycled around
